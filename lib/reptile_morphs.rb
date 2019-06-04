47788251a45a804e7fc6d86ee01052d534d4d2fe
Travis Exline <t.exline22@gmail.com>
2019-06-04 17:41:47 +0000

    first commit

---
+++ lib/reptile_morphs.rb
@@ -0,0 +1,6 @@
+require "reptile_morphs/version"
+
+module ReptileMorphs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/reptile_morphs/version.rb
@@ -0,0 +1,3 @@
+module ReptileMorphs
+  VERSION = "0.1.0"
+end
